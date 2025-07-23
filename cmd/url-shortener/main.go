102b0635866e314b3afe941e6352b08d1b158048
IOSA33 <sedinkin33@gmail.com>
2025-07-23 21:25:02 +0300

    slog function done

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,47 @@
 package main
 
-func main() {
-	// TODO: init config: cleanenv
+import (
+	"log/slog"
+	"os"
+	"rest-api/internal/config"
+)
 
-	// TODO: init logger: slog
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// init config: cleanenv
+	cfg := config.MustLoad()
+
+	// init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("Starting URL Shortener ", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: SQLite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Config struct {
-	Env         string `yaml:"env" env:"ENV" env-default:"local"`
+	Env         string `yaml:"env" env-default:"local"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
@@ -21,9 +22,11 @@ type HTTPServer struct {
 func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		log.Fatal("CONFIG_PATH is not set")
+		log.Println("CONFIG_PATH is not set, using default: ./config/local.yaml")
+		configPath = "./config/local.yaml"
 	}
 
+	// check if file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("config file does not exist: %s", configPath)
 	}
