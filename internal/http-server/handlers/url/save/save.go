1e137e7db8eaa17bfe178a338db5eb857a831200
IOSA33 <sedinkin33@gmail.com>
2025-08-01 13:50:18 +0300

    added tests and mocks to save directory

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -26,6 +26,7 @@ type Response struct {
 	Alias string `json:"alias,omitempty"`
 }
 
+//go:generate go run github.com/vektra/mockery/v3@v3.5.1 --help
 type URLSaver interface {
 	SaveURL(urlToSave string, alias string) (int64, error)
 }
---
+++ internal/lib/logger/handler/slogdiscard/slogdiscard.go
@@ -0,0 +1,39 @@
+package slogdiscard
+
+import (
+	"context"
+	"log/slog"
+)
+
+// slogdiscard is a method for tests that does not write
+// logs down, so it won't write any log in console
+
+func NewDiscardLogger() *slog.Logger {
+	return slog.New(NewDiscardHandler())
+}
+
+type DiscardHandler struct{}
+
+func NewDiscardHandler() *DiscardHandler {
+	return &DiscardHandler{}
+}
+
+func (h *DiscardHandler) Handle(_ context.Context, _ slog.Record) error {
+	// ignore list
+	return nil
+}
+
+func (h *DiscardHandler) WithAttrs(_ []slog.Attr) slog.Handler {
+	// return empty list
+	return h
+}
+
+func (h *DiscardHandler) WithGroup(_ string) slog.Handler {
+	// returns empty list
+	return h
+}
+
+func (h *DiscardHandler) Enabled(_ context.Context, _ slog.Level) bool {
+	// always returns false, because no items to save in log
+	return false
+}
